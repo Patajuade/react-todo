4592bdfe193035d026afb37cc3d967794210f982
Frédéric Pluquet <fredericpluquet@gmail.com>
2022-11-19 15:49:23 +0100

    More work on solution

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,12 @@
 import React from 'react';
+import logo from '../assets/logoHELHa.jpg';
+import styles from './About.module.css';
 
-const About = () => <div>hello from about page</div>;
+const About = () => (
+  <div className={styles.mainDiv}>
+    <h1>Projet React de la HELHa</h1>
+    <img src={logo} alt="Logo de la HELHa" />
+    <div>Par Pluquet Frédéric</div>
+  </div>
+);
 export default About;
