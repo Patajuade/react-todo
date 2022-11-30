f991da71689d69633421637304c0131c4f555959
Patajuade <manon.diana@icloud.com>
2022-11-30 08:49:26 +0100

    add about page html and css

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,4 +1,13 @@
 import React from 'react';
+import '../App.css';
+import image from './helha.png';
 
-const About = () => <div>hello from about page</div>;
+const About = () => (
+  <div className="aboutContainer">
+    <div className="aboutName">Manon </div>
+    <div className="aboutName">Canavaggio-Diana </div>
+    <img className="aboutImage" src={image} alt="helha Img" width="400" height="200" />
+    <div className="aboutYear">Année académique 2022-2023</div>
+  </div>
+);
 export default About;
